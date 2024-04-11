7ebf800bb75956fd952a20ae7185c89b6288842f
Gala Karina <galakarinareyes@gmail.com>
2024-04-11 15:43:09 -0600

    control props alternativo

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -11,19 +11,13 @@ const useProduct = ( { onChange , product, value = 0}: useProductArgs) => {
 
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef( !!onChange )    
 
     useEffect(() => {
       setCounter(value);   
     }, [value])
     
 
-    const increaseBy = ( value : number) => {
-
-      if(isControlled.current){
-        return onChange!({count: value, product})
-      }
-
+    const increaseBy = ( value : number) => {     
       
       const newValue = Math.max( counter + value, 0)
         setCounter(newValue);
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,40 +9,26 @@ const useShoppingCart = () => {
 
   const onProductCountChange = ({count, product}: {count:number, product:Product}) => { 
 
-    setShoppingCart(oldShoppingCart => {
+    setShoppingCart(oldShoppingCart => {     
 
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count:0};
-
-      if(Math.max(productInCart.count + count,0)>0){
-        productInCart.count += count;
-        return{
-          ...oldShoppingCart,
-          [product.id]: productInCart
-        }
+      if(count === 0){
+       const { [product.id]: toDelete, ...rest} = oldShoppingCart;
+        return rest;
+        
       }
 
-      //Borrar el producto
-      const { [product.id]: toDelete, ...rest} = oldShoppingCart;
-      return rest;
-
-      // if(count === 0){
-      //  const { [product.id]: toDelete, ...rest} = oldShoppingCart;
-        // return rest;
-        
-      // }
-
-      // return{
-      //   ...oldShoppingCart,
-      //   [product.id]:{...product, count}
-      // }
+      return{
+        ...oldShoppingCart,
+        [product.id]:{...product, count}
+      }
     })
     
   }
 
   return {
-    onProductCountChange,    
+    products,
     shoppingCart,
-    products
+    onProductCountChange,    
   }
 }
 
